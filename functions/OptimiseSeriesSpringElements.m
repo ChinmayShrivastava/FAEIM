1f3a98b5abca821482f1bd857cb5836a22c07acf
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2021-06-24 00:21:31 +0530

    Returns series equivalent stiffness and the number of elements used
    The function assembles spring elements in series for required displacement, given the maximum elastic deformation of individual spring element.

---
+++ functions/OptimiseSeriesSpringElements.m
@@ -0,0 +1,18 @@
+function y = OptimiseSeriesSpringElements(umax, uneed, k, n)
+%===============================
+%to achieve a given displacement
+%===============================
+%n is the number of elements available
+%umax is the maximum deformation the spring can sustain
+%k is the individual spring stiffness
+%uneed is the displacement required
+
+e_ideal = round(uneed/umax) + 1;%additional element for tolerance
+k_required = k/e_ideal;%required equivalent stiffness
+
+%returning the final equivalent stiffness and number of additional elements required
+if n>=e_ideal
+    y = [k_required, e_ideal];
+else
+    y = [k/n, n];
+end
