b43fb8487123536f2de418189f9f81d7e3e1fe73
Chinmay Shrivastava <79005030+ChinmayShrivastava@users.noreply.github.com>
2021-06-28 23:09:38 +0530

    Updated the bar element optimisation function

--- LinearBarElement/functions/OptimizeParallelLinearBarElement.m
+++ LinearBarElement/functions/OptimizeParallelLinearBarElement.m
@@ -1,4 +1,4 @@
-function y = OptimizeParallelLinearBarElement(F, E, A_existing, L_existing, dimn) %number of elements required for given arrangement
+function y = OptimizeParallelLinearBarElement(F, sigmabreak, E, dimn_one_ele, l_min, u_req) %number of elements required for given arrangement
                                                                  %remove extra elements
                                                                  %add more
                                                                  %if
@@ -11,9 +11,47 @@ r = sqrt(A/pi);
 y = 2*r;
 z = 2*r;
 
-x_lim = dimn(1);
-y_lim = dimn(2);
-z_lim = dimn(3);
+x_lim = dimn_one_ele(1);
+y_lim = dimn_one_ele(2);
+z_lim = dimn_one_ele(3);
+
+A_min = F/sigmabreak; %1
+%assuming circular cross-section
+r_max = (y_lim>=z_lim)*z_lim+(y_lim<z_lim)*y_lim; %3
+A_max = pi*(r_max)*(r_max); %2
+k_req = F/u_req; %4
+
+k_max = E*A_max/l_min;%8
+boolean = k_max<k_req;%7
+if boolean==1 %5
+    e = round(k_req/k_max);%9
+    K = e*k_max;%10
+    if K<k_req%11
+        K = K+k_max;%12
+        e = e+1;%14
+    end%13
+elseif boolean==0%15
+    l_opt = E*A_max/k_req;%16 %maximize A for better tolerance
+    if l_opt<=x_lim%17
+        K = E*A_max/l_opt;%19
+    elseif l_opt>x_lim%20
+        l_opt = x_lim;%21
+        A_opt = k_req*l_opt/E;%22
+        if A_opt>=A_min%23
+            K = E*A_opt/l_opt;%24
+        else%25
+            k_min = E*A_min/l_opt;%26
+            while true%27
+                e = 2;%28
+                if k_min/e<=k_req%29
+                    break%30
+                else%31
+                    e = e+1;%32
+                end%33
+            end%29
+        end%27
+    end%18
+end %6
 
 if x>x_lim
 elseif y>y_lim
